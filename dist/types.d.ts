da041dcd1a1c7bb64e185ec1044580eea232fbcf
Mathis Hofer <hofer@puzzle.ch>
2020-11-25 11:36:12 +0100

    gitpkg

---
+++ dist/types.d.ts
@@ -0,0 +1,14 @@
+/**
+ * Announces the message using screen reader technology.
+ */
+export function announce(message: string, assertiveness?: string, timeout?: number): void;
+/**
+ * Stops all queued announcements.
+ */
+export function clearAnnouncer(assertiveness: any): void;
+/**
+ * Removes the announcer from the DOM.
+ */
+export function destroyAnnouncer(): void;
+
+//# sourceMappingURL=types.d.ts.map
